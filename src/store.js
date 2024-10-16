8baeabcc5c95e7571092491036b13075f38b8fe3
Ladipo Layode <ladipol@gmail.com>
2024-10-16 11:46:53 +0100

    userSlice setup and navbar refactor

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./features/cart/CartSlice";
+import userReducer from "./features/users/userSlice";
 
 export const store = configureStore({
 	reducer: {
 		cartState: cartReducer,
+		userState: userReducer,
 	},
 });
